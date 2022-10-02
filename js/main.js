7c75735247002b9cfe12e9a77cb398b2ed8af7bb
Arafat Hossaine <arafathossain7960@gmail.comm>
2022-10-02 15:27:27 +0600

    check all function, it is working good, ready to deploy

--- js/main.js
+++ js/main.js
@@ -5,52 +5,47 @@
 
 // select player function --------------
 function selectPlayer(name, id){
-    console.log(name, id)
-    const selectedPlayer = document.getElementById('selected');
-    const li = document.createElement('li');
-    li.innerText = name;
-    selectedPlayer.appendChild(li) ;
+    if(allPlayers() >= 5){
+        alert('You have already added 5 players!!! ')
+    }else{
+        const showedPlayer = document.getElementById('selected');
+        const li = document.createElement('li');
+        li.innerText = name;
+        showedPlayer.appendChild(li) ;
+        
+        document.getElementById(id).setAttribute('disabled', true)
+        document.getElementById(id).setAttribute('style','background-color:gray')
     
- 
-    document.getElementById(id).setAttribute('disabled', true)
-    document.getElementById(id).setAttribute('style','background-color:gray')
-
-    // // document.getElementsByClassName(select).setAttribute('disabled', true)
-    // document.getElementsByClassName(select).setAttribute('style','background-color:black')
+    }
+   
 }
 
-// calculate button listener 
+// calculate button listener -----------
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const playerRate = convertToInt('player-rate', true);
     if(playerRate !=='' && playerRate > 0){
 
     const playerCost = document.getElementById('player-cost');
-    playerCost.innerText = playerRate * 5;
+    playerCost.innerText = playerRate * allPlayers();
      } else{
         alert('please!! enter number,')
     }
 
 })
 
-/// total cost calculation 
+/// total cost calculation ------------------
 document.getElementById('total-calculate-btn').addEventListener('click', function(){
-
     const playerCost = convertToInt('player-cost', false);
-    
         const managerCost = convertToInt('manager-cost', true);
         const couchCost = convertToInt('couch-cost', true);
         const totalCost = playerCost + managerCost + couchCost;
         document.getElementById('total-cost').innerText = totalCost;
-        console.log(totalCost)
-   
-    
-
-  
-   
+        
 })
 
 
-// shared or common function ---------
+//---------------- shared or common function -------------------
+// get value from id and convert string to integer
 function convertToInt(id,  isIdInput){
     if(isIdInput === true){
         const inputValue = document.getElementById(id).value;
@@ -61,11 +56,16 @@ function convertToInt(id,  isIdInput){
             alert('please!! enter number,')
         }
         
-        
     }else{
         const textValue = document.getElementById(id).innerText;
         return textNumber = parseInt(textValue);
     }
 } 
 
+// ----- Get childNodList from ol
 
+function allPlayers(){
+    const players = document.querySelectorAll('li');
+    return  players.length;
+   
+}
